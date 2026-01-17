2eaf98a27e1c99d7e6ca550050b85181209cf4cc
Starship100 <jimmy.widlund@outlook.com>
2026-01-17 23:22:01 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+username = "Jimmy"
+value = 100
+
+print("Hello " + username)
+print("Mitt namn är Jimmy")
+
+print("Talet är " + str(value))
+
+first_value = input("Skriv in ett tal: ")
+second_value = input("Skriv in ett till tal: ")
+first_value_int = int(first_value)
+second_value_int = int(second_value)
+#summary = first_value_int + second_value_int
+
+#print("Summan är: " + str(summary))
+print("Summan är: " + str(first_value_int + second_value_int))
+
+print("Hej då " + username)
