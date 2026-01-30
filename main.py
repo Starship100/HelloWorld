5efcfb3e1beefa64932db87224bc5ed259a10e46
Starship100 <jimmy.widlund@outlook.com>
2026-01-30 15:49:58 +0100

    Intro

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ print("Mitt namn är Jimmy")
 
 print("Talet är " + str(value))
 
-first_value = input("Skriv in ett tal: ")
-second_value = input("Skriv in ett till tal: ")
-first_value_int = int(first_value)
-second_value_int = int(second_value)
-#summary = first_value_int + second_value_int
+first_value = int(input("Skriv in ett tal: "))
+second_value = int(input("Skriv in ett till tal: "))
+#first_value_int = int(first_value)
+#second_value_int = int(second_value)
+#summary = first_value + second_value
 
 #print("Summan är: " + str(summary))
-print("Summan är: " + str(first_value_int + second_value_int))
+print("Summan är: " + str(first_value + second_value))
 
 print("Hej då " + username)
